9ed28148993129b218cee60cdbbab2cd2c3117f7
Kyubu-77 <kobler@fbihome.de>
2021-04-13 22:57:02 +0200

    First

---
+++ Program.vb
@@ -0,0 +1,36 @@
+Imports System
+Imports GemBox.Pdf
+Imports GemBox.Pdf.Forms
+
+Module Program
+    Sub Main(args As String())
+        'Using limited key here leads to "Free version limitation has been exceeded (Free version is limited to 2 pages). ..." error. 
+        'Bug reproducible with normal key
+        ComponentInfo.SetLicense("FREE-LIMITED-KEY")
+        TestNullRefenceException()
+
+        Console.WriteLine("Done!")
+    End Sub
+
+
+
+    Function TestNullRefenceException() As Integer
+        Using document = PdfDocument.Load("..\..\..\Antrag.pdf")
+            'Test with CR LF and CRLF
+
+            Dim new_Test As String = "Greg Tester"
+            'fill Textfeld 1 which shows the error
+            Dim formField As PdfField = document.Form.Fields("Textfeld 1")
+            Dim tmp As PdfTextField = formField
+            tmp.Value = new_Test
+
+            document.Save("..\..\..\Antrag_Filled.pdf")
+            document.Close()
+        End Using
+        Return 0
+    End Function
+
+
+
+
+End Module
